608d3fd14078ec3e54c22a8be018f795f3357de1
Aljay Caraballido <jaycaraballido@gmail.com>
2024-12-01 18:11:36 +0800

    Add files via upload

---
+++ Landing page/js.js
@@ -0,0 +1,6 @@
+const design = document.getElementById("design")
+const navlist = document.getElementById("navlist")
+
+design.addEventListener("click", ()=>{
+    navlist.classList.toggle("navlist-active")
+})
