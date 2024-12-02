c54ae0313daf90a5d8bf19d009faa7d9852754ca
Aljay Caraballido <jaycaraballido@gmail.com>
2024-12-02 12:19:45 +0800

    Delete Landing page directory

--- Landing page/js.js
+++
@@ -1,6 +0,0 @@
-const design = document.getElementById("design")
-const navlist = document.getElementById("navlist")
-
-design.addEventListener("click", ()=>{
-    navlist.classList.toggle("navlist-active")
-})
